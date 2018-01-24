089ce2550318c467b8da8154274850dacb7a1f5d
Kohei Matsumoto <34151961+matzkoh@users.noreply.github.com>
2018-01-24 23:35:07 +0900

    Initial commit

---
+++ test/index.spec.js
@@ -0,0 +1,76 @@
+const Koa = require('koa');
+const request = require('supertest');
+
+jest.useFakeTimers();
+
+const keepalive = require('../lib/index');
+
+describe('glitch-keepalive', () => {
+  it('getUrl', () => {
+    expect(keepalive.getUrl('glitch-project-id')).toBe('http://glitch-project-id.glitch.me/keepalive');
+  });
+
+  describe('middleware', () => {
+    let app;
+
+    beforeEach(() => {
+      app = new Koa();
+
+      const getUrl = keepalive.getUrl;
+
+      keepalive.getUrl = jest.fn(() => 'http://localhost:3333/keepalive');
+
+      app.use(keepalive());
+
+      keepalive.getUrl = getUrl;
+    });
+
+    it('routes /keepalive', async () => {
+      await request(app.callback())
+        .get('/keepalive')
+        .expect(200)
+        .expect('Content-Type', 'text/plain; charset=utf-8')
+        .expect('OK');
+    });
+
+    it('get /keepalive every 3 minutes', async () => {
+      const okRoute = jest.fn();
+      const ngRoute = jest.fn();
+
+      app.use(async (ctx, next) => {
+        if (ctx.path ===  '/keepalive')
+          okRoute(ctx);
+
+        else {
+          ngRoute(ctx);
+        }
+
+        return await next();
+      });
+
+      const server = app.listen(3333);
+
+      try {
+        expect(okRoute).not.toBeCalled();
+        expect(ngRoute).not.toBeCalled();
+
+        jest.runOnlyPendingTimers();
+
+        await new Promise(resolve => {
+          app.use(async (ctx, next) => {
+            await next();
+
+            resolve();
+          });
+        });
+
+        expect(okRoute).toHaveBeenCalledTimes(1);
+        expect(ngRoute).not.toBeCalled();
+      } catch (e) {
+        throw e;
+      } finally {
+        await new Promise(resolve => server.close(resolve));
+      }
+    });
+  });
+});
